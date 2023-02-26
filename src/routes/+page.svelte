3332081d138445c805d5e3afc1140dc832fc5f9d
Alex Bethel <abethel@cs.nmt.edu>
2023-02-26 15:03:43 -0700

    I made a silly stub page wheeeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+ import "./main.css";
+</script>
+
+<svelte:head>
+    <title>
+        FerrisLand
+    </title>
+</svelte:head>
+
+<div class="fullpage">
+    <div class="centered">
+        <h1>
+            FerrisLand
+        </h1>
+
+        <p>
+            ...coming soon to a crab near you!
+        </p>
+    </div>
+</div>
+
+<style>
+ .fullpage {
+     background-color: #113455;
+     color: #ffffff;
+     min-height: 100vh;
+ }
+
+ .centered {
+     max-width: 800px;
+     margin: auto;
+     padding: 1px;
+ }
+
+ h1 {
+     font-size: 80px;
+ }
+</style>
